1b63f94f88f82187976c065091937293809d05f4
Jan Klimo <jan.klimo@gmail.com>
2019-01-02 21:49:47 +0700

    Finished chart with all data

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,5 +3,10 @@
 class ChartsController < ApplicationController
   def index
     @chart_data = Entry.order(:created_at).all.pluck(:created_at, :gvt_invested)
+    @entry = Entry.order(:created_at).last
+
+    quote = Quote.order(:created_at).last
+    @usd_amount = quote.usd_amount_for(@entry.gvt_invested).to_i
+    @btc_amount = quote.btc_amount_for(@entry.gvt_invested).to_i
   end
 end
