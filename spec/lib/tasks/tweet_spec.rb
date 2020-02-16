5c9c706a75f9008ad39014abd2ea8b4d8c3229ca
Jan Klimo <jan.klimo@gmail.com>
2020-02-16 19:25:47 +0700

    Update specs to cover Discord calls

--- spec/lib/tasks/tweet_spec.rb
+++ spec/lib/tasks/tweet_spec.rb
@@ -9,15 +9,23 @@ describe 'tweet:post' do
     create(:entry, gvt_invested: 100_000, created_at: 1.day.ago)
 
     allow(IMGKit).to receive(:new).and_return(double(to_file: Tempfile.new))
-    allow_any_instance_of(Twitter::REST::Client).to receive(:update_with_media)
+    @tweet_url = 'https://twitter.com/janklimo/status/1098890150954754048'
+    allow_any_instance_of(Twitter::REST::Client)
+      .to receive(:update_with_media)
+      .and_return(double(uri: Addressable::URI.parse(@tweet_url)))
 
     stub_request(:get, ENV.fetch('HOST'))
       .to_return(status: 200, body: file_fixture('programs.json'))
+
+    allow_any_instance_of(Discordrb::Bot).to receive(:run)
+    allow_any_instance_of(Discordrb::Bot).to receive(:stop)
   end
 
   it 'posts tweet with media' do
     expect_any_instance_of(Twitter::REST::Client)
       .to receive(:update_with_media).with(/102,800 GVT invested/, anything)
+    expect_any_instance_of(Discordrb::Bot)
+      .to receive(:send_message).with(String, @tweet_url)
     task.invoke
   end
 end
